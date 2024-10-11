560a80b0327f508a066581ef20237ad479b5e178
TRACTOPELE <clmntcarriere@gmail.com>
2024-10-11 15:00:59 +0200

    O

---
+++ MVC/model.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+
+app.get('/ping', (req, res) => res.sendStatus(200));
+
+app.listen(3000, () => console.log('Server started on http://localhost:3000'));
